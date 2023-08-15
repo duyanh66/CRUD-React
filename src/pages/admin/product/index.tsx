8cb2576c6485a01d4f3eff36dc3b547c6b3a3e75
duyanh66 <ngduyanh1512@gmail.com>
2023-08-16 00:39:06 +0700

    Thêm sửa xóa bằng react

---
+++ src/pages/admin/product/index.tsx
@@ -0,0 +1,76 @@
+type Props = {};
+import { useGetProductsQuery, useRemoveProductMutation } from "@/api/product";
+import { IProduct } from "@/interfaces/product";
+import { Table, Button, Skeleton, Popconfirm, Alert } from "antd";
+import { Link } from "react-router-dom";
+
+
+const AdminProduct = (props: Props) => {
+    const { data: productData, error, isLoading } = useGetProductsQuery();
+    const [removeProduct, { isLoading: isRemoveLoading, isSuccess: isRemoveSuccess }] =
+        useRemoveProductMutation();
+
+    const confirm = (id: number) => {
+        removeProduct(id);
+    };
+    const dataSource = productData?.map(({ id, name, price }: IProduct) => ({
+        key: id,
+        name,
+        price,
+    }));
+    const columns = [
+        {
+            title: "Tên sản phẩm",
+            dataIndex: "name",
+            key: "name",
+        },
+        {
+            title: "Giá",
+            dataIndex: "price",
+            key: "price",
+        },
+        {
+            title: "",
+            render: ({ key: id }: any) => {
+                return (
+                    <>
+                        <div className="flex space-x-2">
+                            <Popconfirm
+                                title="Bạn chắc chắn muốn xóa ??"
+                                onConfirm={() => confirm(id)}
+                                okText="Yes"
+                                cancelText="No"
+                            >
+                                <Button type="primary" danger>
+                                    Xóa
+                                </Button>
+                            </Popconfirm>
+
+                            <Button type="primary" danger>
+                                <Link to={`/admin/product/${id}/edit`}>Sửa</Link>
+                            </Button>
+                        </div>
+                    </>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div>
+            <header className="mb-4 flex justify-between items-center">
+                <h2 className="font-bold text-2xl">Quản lý sản phẩm</h2>
+                <Button type="primary" danger>
+                    <Link to="/admin/product/add" className="flex items-center space-x-2">
+
+                        Thêm sản phẩm
+                    </Link>
+                </Button>
+            </header>
+            {isRemoveSuccess && <Alert message="Đã xóa thành công" type="success" />}
+            <Table dataSource={dataSource} columns={columns} />
+        </div>
+    );
+};
+
+export default AdminProduct;
